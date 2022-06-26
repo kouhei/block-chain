db32eb6b584efc110ec33898c7763fa1983d49c9
kouhei <kouhei@users.noreply.github.com>
2022-06-27 02:19:10 +0900

    update

--- src/server.ts
+++ src/server.ts
@@ -52,8 +52,30 @@ app.get('/chain', (_req, res) => {
   res.send(body);
 });
 
-// TODO: コンセンサスアルゴリズムのためのエンドポイント実装
-// https://qiita.com/hidehiro98/items/841ece65d896aeaa8a2a#%E6%96%B0%E3%81%97%E3%81%84%E3%83%8E%E3%83%BC%E3%83%89%E3%82%92%E7%99%BB%E9%8C%B2%E3%81%99%E3%82%8B
+app.post('/nodes/register', (req, res) => {
+  const nodes = req.body.nodes as string[]; // TODO: 要素が一つだけの時にstringとstring[]を区別
+  if (!nodes) {
+    res.status(400).send('invalid nodes');
+  }
+
+  nodes.forEach((node) => blockChain.registerNode(node));
+
+  res.status(201).json({
+    message: '新しいノードが追加されました',
+    totalNodes: blockChain.nodes.values,
+  });
+});
+
+app.get('/nodes/resolve', async (req, res) => {
+  const isReplaced = await blockChain.resolveConflicts();
+  res.status(200).json({
+    message: isReplaced ? 'チェーンが置き換えられました' : 'チェーンが確認されました',
+    chain: blockChain.chain,
+  });
+});
+
+// TODO: コンセンサスアルゴリズムのためのエンドポイント実装の動作確認から
+// https://qiita.com/hidehiro98/items/841ece65d896aeaa8a2a#%E3%82%B3%E3%83%B3%E3%82%BB%E3%83%B3%E3%82%B5%E3%82%B9%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E3%82%92%E5%AE%9F%E8%A3%85%E3%81%99%E3%82%8B
 
 const port = 3000;
 app.listen(port, () => {
