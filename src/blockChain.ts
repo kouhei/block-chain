587c077cd35f5ab4088ccc679f2593befaa4e4a8
kouhei <kouhei@users.noreply.github.com>
2022-06-26 20:09:16 +0900

    update

--- src/blockChain.ts
+++ src/blockChain.ts
@@ -1,3 +1,5 @@
+import {createHash} from "crypto";
+
 type Uuid = string;
 type Amount = number;
 class Transaction{
@@ -23,28 +25,29 @@ class Block{
 }
 
 class BlockChain{
-    private #chain: Block[];
-    private #current_transactions: Transaction[];
+    private chain: Map<Index,Block>;
+    private current_transactions: Transaction[];
 
     constructor(){
-        this.#chain = [];
-        this.#current_transactions = [];
+        this.chain = new Map();
+        this.current_transactions = [];
     }
 
-    /** 
+    /**
      * create a new block and add it chain
      * return new block
-     */ 
+     */
     private addNewBlockToChain(proof: Block["proof"], previousHash: Block["previousHash"] = null):Block{
+        const index = this.chain.size + 1;
         const newBlock = new Block(
-            this.#chain.length+1,
+            index,
             Date.now(),
-            this.#current_transactions,
+            this.current_transactions,
             proof,
-            previousHash || BlockChain.hash(this.#chain[this.#chain.length - 1])
+            previousHash || BlockChain.hash(this.chain[this.chain.size - 1])
         );
-        this.#current_transactions = [];
-        this.#chain.push(newBlock);
+        this.current_transactions = [];
+        this.chain.set(index, newBlock);
         return newBlock;
     }
 
@@ -53,16 +56,22 @@ class BlockChain{
      * return address of the next block
      */
     private newTransaction(sender: Transaction["sender"], recipient: Transaction["recipient"], amount: Transaction["amount"]):Number{
-        this.#current_transactions.push(
+        this.current_transactions.push(
             new Transaction(sender, recipient, amount)
         );
         return this.lastBlock.index + 1;
     }
 
-    static hash(block: Block):Hash {
+    static generateHash(block: Block):Hash {
+        // TODO: ここから　https://qiita.com/hidehiro98/items/841ece65d896aeaa8a2a#%E6%96%B0%E3%81%97%E3%81%84%E3%83%96%E3%83%AD%E3%83%83%E3%82%AF%E3%82%92%E4%BD%9C%E3%82%8B
+        const blockMap = new Map(Object.entries(block));
+        const jsonStr = JSON.stringify(Array.from(blockMap));
+        const hash = createHash('sha256');
+        hash.update(jsonStr);
+        return hash.digest('hex')
     }
 
     get lastBlock(): Block {
-        return this.#chain[this.#chain.length - 1];
+        return this.chain.get(this.chain.size - 1);
     }
 }
