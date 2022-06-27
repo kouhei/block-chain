80425d2dc7fb5ffb33be1820281999e34b617506
kouhei <kouhei@users.noreply.github.com>
2022-06-27 21:04:48 +0900

    delete blockChain

--- src/core/application/services/blockChain.ts
+++
@@ -1,7 +0,0 @@
-// TODO: blockChainServiceでメソッドを持つ。
-class BlockChain {
-  /** ブロックとして追加されていないトランザクション */
-  // private current_transactions: ITransactions;
-}
-
-// export const blockChain = new BlockChain();
