622431c463dfc272c092567d16329362f33422bf
kouhei <kouhei@users.noreply.github.com>
2022-06-27 13:11:26 +0900

    update

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import * as bodyParser from 'body-parser';
 import { v4 as uuidv4 } from 'uuid';
 import { blockChain } from './blockChain';
 
+const port = Number.parseInt(process.argv[2]) || 3000;
+
 const node_id = uuidv4();
 
 const app = express();
@@ -56,6 +58,7 @@ app.post('/nodes/register', (req, res) => {
   const nodes = req.body.nodes as string[]; // TODO: 要素が一つだけの時にstringとstring[]を区別
   if (!nodes) {
     res.status(400).send('invalid nodes');
+    return;
   }
 
   nodes.forEach((node) => blockChain.registerNode(node));
@@ -66,18 +69,18 @@ app.post('/nodes/register', (req, res) => {
   });
 });
 
+// TODO: chain/resolveの方が良くない？
 app.get('/nodes/resolve', async (req, res) => {
   const isReplaced = await blockChain.resolveConflicts();
   res.status(200).json({
     message: isReplaced ? 'チェーンが置き換えられました' : 'チェーンが確認されました',
-    chain: blockChain.chain,
+    chain: Object.fromEntries(blockChain.chain.entries()),
   });
 });
 
 // TODO: コンセンサスアルゴリズムのためのエンドポイント実装の動作確認から
 // https://qiita.com/hidehiro98/items/841ece65d896aeaa8a2a#%E3%82%B3%E3%83%B3%E3%82%BB%E3%83%B3%E3%82%B5%E3%82%B9%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0%E3%82%92%E5%AE%9F%E8%A3%85%E3%81%99%E3%82%8B
 
-const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
